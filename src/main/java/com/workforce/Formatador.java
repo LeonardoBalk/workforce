07113b8fe8c774b8d8b97ce0e1d5fdeee797625f
leonardo <leonardo.balk01@gmail.com>
2026-09-14 17:10:15 -0300

    feat: add Pessoa, Funcionario and formatter classes

---
+++ src/main/java/com/workforce/Formatador.java
@@ -0,0 +1,23 @@
+package com.workforce;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class Formatador {
+
+    private static final DateTimeFormatter FORMATO_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public static String formatarData(LocalDate data) {
+        return data.format(FORMATO_DATA);
+    }
+
+    public static String formatarValor(BigDecimal valor) {
+        NumberFormat formato = NumberFormat.getNumberInstance(new Locale("pt", "BR"));
+        formato.setMinimumFractionDigits(2);
+        formato.setMaximumFractionDigits(2);
+        return formato.format(valor);
+    }
+}
---
+++ src/main/java/com/workforce/Funcionario.java
@@ -0,0 +1,41 @@
+package com.workforce;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+
+    private final String funcao;
+    private BigDecimal salario;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void aumentarSalario(BigDecimal percentual) {
+        salario = salario.add(salario.multiply(percentual));
+    }
+
+    public BigDecimal calcularSalariosMinimos(BigDecimal salarioMinimo) {
+        return salario.divide(salarioMinimo, 2, RoundingMode.HALF_UP);
+    }
+
+    @Override
+    public String toString() {
+        return getNome()
+                + " | Nascimento: " + Formatador.formatarData(getDataNascimento())
+                + " | Salário: " + Formatador.formatarValor(salario)
+                + " | Função: " + funcao;
+    }
+}
